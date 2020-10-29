83b69ed63e32208ab46bc1c76220a34e29ca762f
matheusm0ura <matheusdj49@live.com>
2020-10-29 17:45:36 -0300

    Update CaesarCipher.py

--- Exercícios/CaesarCipher.py
+++ Exercícios/CaesarCipher.py
@@ -3,11 +3,11 @@ def encrypt(s, k):
 
     aux = ""
     for letter in ls:
-    if not letter.isalpha():
-        aux += letter
-    elif letter.isupper():
-        aux += chr((ord(letter) + k-65) % 26 + 65)
-    elif letter.islower():
-        aux += chr((ord(letter) + k-97) % 26 + 97)
+        if not letter.isalpha():
+            aux += letter
+        elif letter.isupper():
+            aux += chr((ord(letter) + k-65) % 26 + 65)
+        elif letter.islower():
+            aux += chr((ord(letter) + k-97) % 26 + 97)
     
     print(aux)
